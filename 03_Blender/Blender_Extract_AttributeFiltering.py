dab0e747d2e259338334a64838e9bd2fe33aa44a
clelambin <cle.lambin@gmail.com>
2025-06-18 15:06:54 +0200

    [Blender] Add remesh and green plant function

--- 03_Blender/Blender_Extract_AttributeFiltering.py
+++ 03_Blender/Blender_Extract_AttributeFiltering.py
@@ -113,10 +113,25 @@ def keep_biggest_cluster(mesh:bmesh.types.BMesh) -> None:
     # Delete verts outside of face cluster
     delete_vertices(mesh, list(vert_outside))
 
+def remesh_block_modifier(obj:bpy.types.Object, octree_depth:int=8) -> None:
+    """Use block remesh modifier to simplify the geometry and remove unconnected"""
+    # Add remesh modifier
+    modifier = obj.modifiers.new(name="Remesh", type="REMESH")
+    modifier.mode = "BLOCKS"
+    modifier.scale = 0.9
+    modifier.octree_depth = octree_depth
+    # Make input object as active and switch to object mode to apply the modifier
+    bpy.context.view_layer.objects.active = obj
+    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+    # Apply modifier
+    bpy.ops.object.modifier_apply(modifier="Remesh")
+
 def extract_component(name:str="Pot",
                       color_selection:Callable=rgba_to_grey,
-                      color_min:float=0,
-                      color_max:float=float("Inf")) -> bpy.types.Object:
+                      delete_min:float=0,
+                      delete_max:float=float("Inf"),
+                      filtering_mth:str|None=None,
+                      remesh_octree:float|None=0.8) -> bpy.types.Object:
     """Duplicate active object and keep only selected component (selected by color range)"""
     # Duplicate active object
     bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
@@ -125,16 +140,22 @@ def extract_component(name:str="Pot",
     (obj, obj_mesh) = edit_active_object(name=name)
     # Get list of non-pot vertices (with non-dark color)
     vertices_nonpot = vertex_by_attribute(obj_mesh, "Col",
-                                          min_=color_min,
-                                          max_=color_max,
+                                          min_=delete_min,
+                                          max_=delete_max,
                                           layer="float_color",
                                           transf=color_selection)
     # Delete these vertices
     delete_vertices(obj_mesh, vertices_nonpot)
     # From remaining vertices, keep biggest face cluster
-    keep_biggest_cluster(obj_mesh)
-    # Switch back to object mode
-    bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+    if filtering_mth == "remesh":
+        # Switch back to object mode to apply block remesh
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+        remesh_block_modifier(obj, octree_depth=remesh_octree)
+    elif filtering_mth == "cluster":
+        # Stay in edit mode, then switch to object mode after face clustering
+        keep_biggest_cluster(obj_mesh)
+        bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+
     # Return the extracted object
     return obj
 
@@ -143,14 +164,33 @@ def copy_pot() -> tuple[bpy.types.Object, bpy.types.Object]:
     # Keep active object in memory (for second duplication)
     source_obj = bpy.context.active_object
     # Extract pot
-    pot = extract_component(name="Pot", color_selection=rgba_to_grey, color_min=0.1)
+    pot = extract_component(name="Pot",
+                            color_selection=rgba_to_grey,
+                            delete_min=0.1,
+                            filtering_mth="cluster")
     # Marke original active object as active and perform second copy
     bpy.ops.object.select_all(action='DESELECT')
     bpy.context.view_layer.objects.active = source_obj
     source_obj.select_set(True)
-    cup = extract_component(name="Cup", color_selection=filter_pure_red, color_min=-0.1)
+    cup = extract_component(name="Cup",
+                            color_selection=filter_pure_red,
+                            delete_min=-0.1,
+                            filtering_mth="remesh",
+                            remesh_octree=8)
     # Return list of generated object
     return (pot, cup)
+
+def copy_green_plant(source_obj:bpy.types.Object) -> bpy.types.Object:
+    """Extract non-black part of the source model to get green parts"""
+    # Mark plant as active and selected
+    bpy.ops.object.select_all(action='DESELECT')
+    bpy.context.view_layer.objects.active = source_obj
+    source_obj.select_set(True)
+    # Create copy of plant excluding pot
+    plant_green = extract_component(name="Plant_green",
+                                    color_selection=rgba_to_grey,
+                                    delete_max=0.1)
+    return plant_green
 
 def delete_low_confidence() -> None:
     """Delete vertices with low confidence value from active object"""
@@ -166,7 +206,9 @@ def delete_low_confidence() -> None:
     bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
 
 if __name__ == "__main__":
-    # Test removing low confidence
-    delete_low_confidence()
-    # Test pot detection
-    copy_pot()
+#    # Test removing low confidence
+#    delete_low_confidence()
+#    # Test pot detection
+#    copy_pot()
+    obj = bpy.context.active_object
+    copy_green_plant(obj)
